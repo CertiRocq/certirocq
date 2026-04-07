7a98b2a3172ad4b2604674390b7f2ee72bb30abf
zoep <zoe.paraskevopoulou@gmail.com>
2026-04-07 12:15:34 +0300

    Prove computational ANF top-level correctness

--- theories/LambdaBox_to_LambdaANF/anf_toplevel.v
+++ theories/LambdaBox_to_LambdaANF/anf_toplevel.v
@@ -6,7 +6,8 @@ From MetaRocq.Erasure Require Import
   EAst EAstUtils EGlobalEnv EWellformed EPrimitive.
 From MetaRocq.Common Require Import BasicAst Kernames.
 From compcert Require Import lib.Maps lib.Coqlib.
-From CertiRocq.Common Require Import AstCommon.
+From ExtLib Require Import Structures.Monads.
+From CertiRocq.Common Require Import AstCommon compM.
 From CertiRocq Require Import Pipeline_utils.
 From CertiRocq.LambdaANF Require Import
   cps cps_show eval ctx logical_relations
@@ -17,6 +18,8 @@ From CertiRocq.LambdaBox_to_LambdaANF Require Import
   common ANF fuel_sem wf anf_corresp anf_util anf_correct anf_global.
 
 Import ListNotations.
+Import Monad.MonadNotation.
+Open Scope monad_scope.
 
 Section Refinement.
 
@@ -165,10 +168,9 @@ Section Refinement.
       Sg (List.rev Σ) [] cmap C_env Sg' ->
     anf_cvt_rel func_tag default_tag tgm cmap
       Sg' e [] S' C r ->
-    Disjoint _ (cmap_vars cmap) Sg ->
     exists M, refines M e (C_env |[ C |[ Ehalt r ]| ]|).
   Proof.
-    intros Hwf Hglob_cvt Hmain_cvt Hdis_glob.
+    intros Hwf Hglob_cvt Hmain_cvt.
     pose proof (@anf_cvt_rel_global_complete_top
                   func_tag default_tag tgm efl Σ Hwf_glob HnoAxioms
                   Sg cmap C_env Sg' Hglob_cvt)
@@ -184,7 +186,7 @@ Section Refinement.
     pose proof (@global_ctx_cmap_eval_coherent_top
                   func_tag default_tag tgm cmap Σ efl HnoAxioms
                   box_dc box_tag Hglob_term Hwf_glob
-                  C_env Sg Sg' Hglob_cvt Hdis_glob)
+                  C_env Sg Sg' Hglob_cvt)
       as Hcmap_eval_coherent.
     pose (val_rel_exists :=
       @anf_val_rel_exists func_tag default_tag prim_map tgm prims cmap
@@ -204,7 +206,7 @@ Section Refinement.
                 HnoVar HnoEvar HnoCoFix HnoLazy Hblocks HnoArray
                 no_prims
                 C_env Sg Sg'
-                Hglob_cvt Hdis_glob (M.empty val))
+                Hglob_cvt (M.empty val))
       as [rho_g [F_glob [T_glob [Hglob_rho Hpre_glob]]]].
     exists (T_glob + 1).
     intros src_v f t Heval.
@@ -227,10 +229,12 @@ Section Refinement.
     { intros i x k decl body Hi. destruct i; discriminate. }
     assert (Hdis_nil : Disjoint _ (FromList []) Sg').
     { rewrite FromList_nil. now apply Disjoint_Empty_set_l. }
-    assert (Hdis_main : Disjoint _ (cmap_vars cmap) Sg').
-    { eapply Disjoint_Included_r.
-      - eapply anf_cvt_global_subset. exact Hglob_cvt.
-      - exact Hdis_glob. }
+	    assert (Hdis_main : Disjoint _ (cmap_vars cmap) Sg').
+	    { eapply anf_cvt_global_disjoint_acc.
+	      - exact Hglob_cvt.
+	      - constructor. intros z Hc.
+	        inversion Hc as [? Hz _]; subst; clear Hc.
+	        destruct Hz as [k Hlk]. discriminate. }
     assert (Henv_nil : anf_env_rel0 [] [] rho_g) by constructor.
     assert (Hglob_main : global_env_rel' (kn_deps e) rho_g).
     { intros k v Hdep Hlk.
@@ -270,35 +274,33 @@ Section Refinement.
     { rewrite FromList_nil, Union_Empty_set_neut_l in Hctx_main0.
       exact Hctx_main0. }
 
-    assert (Hr_not_old : ~ r \in (Sg \\ Sg')).
+    assert (Hr_not_old_non_cmap : ~ r \in ((Sg \\ Sg') \\ cmap_vars cmap)).
     { intro Hin_old.
       destruct (@anf_cvt_result_in_consumed
                   func_tag default_tag tgm cmap
                   Sg' e [] S' C r Hmain_cvt)
         as [Hin_vn | [Hin_s | Hin_cm]].
       - rewrite FromList_nil in Hin_vn. contradiction.
-      - exact ((proj2 Hin_old) Hin_s).
-      - eapply (Disjoint_In_l _ _ _ Hdis_glob Hin_cm). exact (proj1 Hin_old). }
+      - exact ((proj2 (proj1 Hin_old)) Hin_s).
+      - exact ((proj2 Hin_old) Hin_cm). }
 
     assert (Hdis_cont :
-      Disjoint _ (occurs_free (C |[ Ehalt r ]|)) (Sg \\ Sg')).
-    { eapply Disjoint_Included_l.
-      - eapply occurs_free_ctx_app.
-      - eapply Union_Disjoint_l.
-        + eapply Disjoint_Included_l.
+      Disjoint _ (occurs_free (C |[ Ehalt r ]|)) ((Sg \\ Sg') \\ cmap_vars cmap)).
+	    { eapply Disjoint_Included_l.
+	      - eapply occurs_free_ctx_app.
+	      - eapply Union_Disjoint_l.
+	        + eapply Disjoint_Included_l.
 	          * exact Hctx_main.
 	          * eapply Union_Disjoint_l.
-	            -- constructor. intros z Hz. inv Hz.
-	               match goal with
-	               | [ Hleft : z \in (Sg' \\ S'), Hright : z \in (Sg \\ Sg') |- _ ] =>
-	                 inv Hleft; inv Hright; contradiction
-	               end.
-	            -- eapply Disjoint_Included_r.
-	               ++ eapply Setminus_Included.
-	               ++ exact Hdis_glob.
-        + eapply Disjoint_Included_l.
-          * rewrite occurs_free_Ehalt. eapply Setminus_Included.
-          * eapply Disjoint_Singleton_l. exact Hr_not_old. }
+	            -- constructor. intros z Hz.
+	               inversion Hz as [? Hleft Hright]; subst; clear Hz.
+	               exact ((proj2 (proj1 Hright)) (proj1 Hleft)).
+	            -- constructor. intros z Hz.
+	               inversion Hz as [? Hleft Hright]; subst; clear Hz.
+	               exact ((proj2 Hright) Hleft).
+	        + eapply Disjoint_Included_l.
+	          * rewrite occurs_free_Ehalt. eapply Setminus_Included.
+	          * eapply Disjoint_Singleton_l. exact Hr_not_old_non_cmap. }
 
 	    assert (Hpre_full :
 	      preord_exp cenv (comp (anf_bound f t) (anf_bound F_glob T_glob))
@@ -333,12 +335,225 @@ Section Refinement.
 
   Theorem anf_correct_top e Sg e_tgt :
     wellformed Σ 0 e = true ->
-    Disjoint _ (cmap_vars cmap) Sg ->
     anf_rel_top e Sg e_tgt ->
     exists M, refines M e e_tgt.
   Proof.
-    intros Hwf Hdis [Sg' [S' [C_env [C [r [Hglob [Hmain ->]]]]]]].
+    intros Hwf [Sg' [S' [C_env [C [r [Hglob [Hmain ->]]]]]]].
     eapply anf_correct_top_explicit; eauto.
   Qed.
 
 End Refinement.
+
+Section ComputationalCorrespondence.
+
+  Context (func_tag default_tag default_itag : positive)
+          (tgm : conId_map)
+          (Σ : EAst.global_context).
+
+  Context {efl : EEnvFlags}.
+  Context (HnoAxioms : has_axioms = false).
+
+  Context (prim_map : M.t primitive)
+          (prims : list (primitive * positive)).
+  Context (Hwf_glob : wf_glob Σ).
+  Context (HnoVar : has_tVar = false)
+          (HnoEvar : has_tEvar = false)
+          (HnoCoFix : has_tCoFix = false)
+          (HnoLazy : has_tLazy_Force = false)
+          (Hblocks : cstr_as_blocks = true)
+          (HnoArray : has_primarray = false).
+  Context (no_prims : forall s, find_prim prims s = None).
+
+  Lemma convert_top_anf_prog_corresp e Sg :
+    wellformed Σ 0 e = true ->
+    compM.triple
+      (fun _ s => fresh Sg (state.next_var (fst s)))
+      ('(cm, C_env) <- convert_global_decls func_tag default_tag prim_map tgm prims (List.rev Σ) [] ;;
+       '(r, C) <- convert_anf func_tag default_tag prim_map tgm prims cm e new_var_map ;;
+       ret (C_env |[ C |[ Ehalt r ]| ]|))
+      (fun _ _ e_tgt _ =>
+         exists cm Sg' S' C_env C r,
+           anf_cvt_rel_global func_tag default_tag tgm
+             Sg (List.rev Σ) [] cm C_env Sg' /\
+           anf_cvt_rel func_tag default_tag tgm cm
+             Sg' e [] S' C r /\
+           e_tgt = C_env |[ C |[ Ehalt r ]| ]|).
+  Proof.
+    intros Hwf.
+    assert (HΣ_top : Σ = List.rev (List.rev Σ) ++ []).
+    { rewrite rev_involutive. rewrite app_nil_r. reflexivity. }
+    assert (Hcm_empty :
+      forall s d, lookup_constant ([] : EAst.global_context) s = Some d ->
+                  lookup_const ([] : const_map) s <> None).
+    { intros s d Hlk. discriminate. }
+    eapply bind_triple.
+    { eapply (@anf_cvt_global_corresp
+                func_tag default_tag prim_map tgm prims
+                efl Σ Hwf_glob HnoAxioms
+                HnoVar HnoEvar HnoCoFix HnoLazy Hblocks HnoArray
+                no_prims
+                (List.rev Σ) [] [] Sg
+                HΣ_top Hcm_empty). }
+    intros [cm C_env] w.
+    eapply pre_existential; intros Sg'.
+    eapply pre_curry_l; intros Hglob_cvt.
+    eapply pre_strenghtening.
+    { intros ? ? [Hcm_complete Hfresh_main].
+      exact (conj Hfresh_main Hcm_complete). }
+    eapply pre_curry_l; intros Hcm_complete.
+    eapply bind_triple.
+    { eapply (@anf_cvt_exp_corresp
+                func_tag default_tag prim_map tgm prims cm
+                efl Σ
+                HnoVar HnoEvar HnoCoFix HnoLazy Hblocks HnoArray
+                no_prims Hcm_complete
+                e [] new_var_map Sg'
+                Hwf var_map_correct_nil). }
+    intros [r C] w'.
+    eapply pre_existential; intros S'.
+    eapply pre_curry_l; intros Hmain_cvt.
+    eapply return_triple. intros _ s _.
+    exists cm, Sg', S', C_env, C, r.
+    repeat split; assumption.
+  Qed.
+
+  Lemma convert_top_anf_corresp next_id ie e e_tgt comp_d' :
+    wellformed Σ 0 e = true ->
+    convert_top_anf func_tag default_tag prim_map default_itag next_id tgm prims
+      ie (List.rev Σ) e = (compM.Ret e_tgt, comp_d') ->
+    exists cm Sg' S' C_env C r,
+      anf_cvt_rel_global func_tag default_tag tgm
+        (fun x => (next_id <= x)%positive) (List.rev Σ) [] cm C_env Sg' /\
+      anf_cvt_rel func_tag default_tag tgm cm
+        Sg' e [] S' C r /\
+      e_tgt = C_env |[ C |[ Ehalt r ]| ]|.
+  Proof.
+    intros Hwf Hrun.
+    unfold convert_top_anf in Hrun.
+    destruct (convert_env default_tag default_itag ie)
+      as [[[[ienv0 cenv0] ctag] itag] dcm].
+    simpl in Hrun.
+    set (ftag := (func_tag + 1)%positive).
+    pose (fenv := M.set func_tag (1%N, (0%N :: nil)) (M.empty _) : fun_env).
+    set (comp_d := state.pack_data next_id ctag itag ftag cenv0 fenv (M.empty _) (M.empty nat) []).
+    set (Sg := fun x => (next_id <= x)%positive).
+    set (prog :=
+      '(cm, C_env) <- convert_global_decls func_tag default_tag prim_map tgm prims (List.rev Σ) [] ;;
+      '(r, C) <- convert_anf func_tag default_tag prim_map tgm prims cm e new_var_map ;;
+      ret (C_env |[ C |[ Ehalt r ]| ]|)).
+    assert (Hprog_corresp :
+      compM.triple
+        (fun _ s => fresh Sg (state.next_var (fst s)))
+        prog
+        (fun _ _ e_out _ =>
+           exists cm Sg' S' C_env C r,
+             anf_cvt_rel_global func_tag default_tag tgm
+               Sg (List.rev Σ) [] cm C_env Sg' /\
+             anf_cvt_rel func_tag default_tag tgm cm
+               Sg' e [] S' C r /\
+             e_out = C_env |[ C |[ Ehalt r ]| ]|)).
+    { unfold prog. eapply convert_top_anf_prog_corresp. exact Hwf. }
+    pose proof Hprog_corresp as Hprog.
+    unfold triple in Hprog.
+    assert (Hfresh : fresh Sg (state.next_var comp_d)).
+    { unfold Sg, comp_d, fresh, Ensembles.In. simpl. lia. }
+    specialize (Hprog tt (comp_d, tt) Hfresh).
+    unfold state.run_compM in Hrun.
+    change
+      ((let '(res_err, (comp_d'', _)) := compM.runState prog tt (comp_d, tt)
+        in (res_err, comp_d'')) = (compM.Ret e_tgt, comp_d')) in Hrun.
+    remember (compM.runState prog tt (comp_d, tt)) as top_run eqn:Htop_run.
+    destruct top_run as [res [comp_d_fin u]].
+    simpl in Hrun, Hprog.
+    destruct res; try discriminate.
+    inversion Hrun; subst; clear Hrun.
+    exact Hprog.
+  Qed.
+
+End ComputationalCorrespondence.
+
+Section ComputationalRefinement.
+
+  Context (func_tag kon_tag default_tag default_itag : positive)
+          (tgm : conId_map)
+          (Σ : EAst.global_context).
+
+  Context {efl : EEnvFlags}.
+  Context (HnoAxioms : has_axioms = false).
+
+  Context (dcon_to_tag_inj :
+    forall dc dc',
+      dcon_to_tag default_tag dc tgm = dcon_to_tag default_tag dc' tgm -> dc = dc').
+
+  Context (box_dc : dcon)
+          (box_tag : dcon_to_tag default_tag box_dc tgm = default_tag).
+
+  Context (Hglob_term :
+    forall k decl body,
+      declared_constant Σ k decl ->
+      decl.(EAst.cst_body) = Some body ->
+      exists src_v f t, @eval_env_fuel nat
+        (LambdaBox_resource_fuel default_tag tgm box_dc box_tag)
+        (LambdaBox_resource_trace default_tag tgm box_dc box_tag)
+        Σ box_dc [] body (Val src_v) f t).
+
+  Context (Hglob_wf :
+    forall k decl body,
+      declared_constant Σ k decl ->
+      decl.(EAst.cst_body) = Some body ->
+      wellformed Σ 0 body = true).
+
+  Context (prim_map : M.t primitive)
+          (prims : list (primitive * positive)).
+  Context (Hwf_glob : wf_glob Σ).
+  Context (HnoVar : has_tVar = false)
+          (HnoEvar : has_tEvar = false)
+          (HnoCoFix : has_tCoFix = false)
+          (HnoLazy : has_tLazy_Force = false)
+          (Hblocks : cstr_as_blocks = true)
+          (HnoArray : has_primarray = false).
+  Context (no_prims : forall s, find_prim prims s = None).
+
+  Definition top_cenv (ie : ienv) : ctor_env :=
+    match convert_env default_tag default_itag ie with
+    | (_, cenv, _, _, _) => cenv
+    end.
+
+  Definition refines_top (ie : ienv) (M : nat) (e_src : EAst.term) (e_tgt : exp) : Prop :=
+    refines default_tag tgm (top_cenv ie) Σ box_dc box_tag M e_src e_tgt.
+
+  Context (ie : ienv).
+  Context (cenv_case_consistent_top :
+    forall P ctag, caseConsistent (top_cenv ie) P ctag).
+
+  Theorem convert_top_anf_correct next_id e e_tgt comp_d' :
+    wellformed Σ 0 e = true ->
+    convert_top_anf func_tag default_tag prim_map default_itag next_id tgm prims
+      ie (List.rev Σ) e = (compM.Ret e_tgt, comp_d') ->
+    exists M, refines_top ie M e e_tgt.
+  Proof.
+    intros Hwf Hrun.
+    edestruct (@convert_top_anf_corresp
+                 func_tag default_tag default_itag
+                 tgm Σ efl HnoAxioms
+                 prim_map prims Hwf_glob
+                 HnoVar HnoEvar HnoCoFix HnoLazy Hblocks HnoArray
+                 no_prims
+                 next_id ie e e_tgt comp_d' Hwf Hrun)
+      as [cm [Sg' [S' [C_env [C [r [Hglob_cvt [Hmain_cvt ->]]]]]]]].
+    eapply (@anf_correct_top_explicit
+              func_tag kon_tag default_tag default_itag
+              tgm cm (top_cenv ie) Σ efl
+              HnoAxioms
+              dcon_to_tag_inj
+              box_dc box_tag
+              cenv_case_consistent_top
+              Hglob_term Hglob_wf
+              prim_map prims Hwf_glob
+              HnoVar HnoEvar HnoCoFix HnoLazy Hblocks HnoArray
+              no_prims
+              e (fun x => (next_id <= x)%positive) Sg' S'
+              C_env C r); eauto.
+  Qed.
+
+End ComputationalRefinement.
