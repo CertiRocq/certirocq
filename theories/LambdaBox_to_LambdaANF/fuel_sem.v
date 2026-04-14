62469c3c9b2c504ebb8bcc6885f38594f6d4c485
zoep <zoe.paraskevopoulou@gmail.com>
2026-04-14 10:55:24 +0300

    Update source fuel semantics and ANF proof support

--- theories/LambdaBox_to_LambdaANF/fuel_sem.v
+++ theories/LambdaBox_to_LambdaANF/fuel_sem.v
@@ -643,6 +643,7 @@ Section FUEL_SEM.
       split; [reflexivity | split; reflexivity].
   Qed.
 
+
   Lemma eval_many_det rho es vs1 vs2 fs1 ts1 fs2 ts2 :
     eval_fuel_many rho es vs1 fs1 ts1 ->
     eval_fuel_many rho es vs2 fs2 ts2 ->
@@ -655,6 +656,27 @@ Section FUEL_SEM.
       injection Heqees as <- <-. f_equal.
       + eapply eval_val_det; eassumption.
       + exact (IHeval_fuel_many _ _ _ H2).
+  Qed.
+
+  Lemma eval_many_exact_det rho es vs1 vs2 fs1 ts1 fs2 ts2 :
+    eval_fuel_many rho es vs1 fs1 ts1 ->
+    eval_fuel_many rho es vs2 fs2 ts2 ->
+    vs1 = vs2 /\ fs1 = fs2 /\ ts1 = ts2.
+  Proof.
+    intros H1 H2. revert vs2 fs2 ts2 H2.
+    induction H1; intros vs2 fs2 ts2 H2.
+    - remember (@nil EAst.term) as es in H2.
+      destruct H2; [repeat split; reflexivity | discriminate].
+    - remember (e :: es) as ees in H2. destruct H2; [discriminate |].
+      injection Heqees as <- <-.
+      match goal with
+      | [ Hval1 : eval_env_fuel _ _ (Val _) _ _,
+          Hval2 : eval_env_fuel _ _ (Val _) _ _ |- _ ] =>
+          pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ Hval1 Hval2)
+            as [-> [-> ->]]
+      end.
+      specialize (IHeval_fuel_many _ _ _ H2) as [-> [-> ->]].
+      repeat split; reflexivity.
   Qed.
 
   Lemma eval_many_app_inv rho args_done e args_rest vs fs ts :
@@ -716,6 +738,335 @@ Section FUEL_SEM.
     diverge rho e -> not_stuck rho e.
   Proof.
     right. assumption.
+  Qed.
+
+  (** Successful whole-application evaluations project to successful prefix
+      evaluations on the function and argument subterms. *)
+  Lemma src_eval_app_val_fun rho e1 e2 v_app f t :
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app) f t ->
+    exists v1 f1 t1,
+      eval_env_fuel rho e1 (Val v1) f1 t1.
+  Proof.
+    intros Happ.
+    inversion Happ; subst; try discriminate.
+    remember (EAst.tApp e1 e2) as e_app in H.
+    remember (Val v_app) as r_app in H.
+    inversion H; subst; try discriminate.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      eexists _, _, _. exact H0.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      eexists _, _, _. exact H0.
+  Qed.
+
+  Lemma src_eval_app_val_arg rho e1 e2 v_app f t :
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app) f t ->
+    exists v2 f2 t2,
+      eval_env_fuel rho e2 (Val v2) f2 t2.
+  Proof.
+    intros Happ.
+    inversion Happ; subst; try discriminate.
+    remember (EAst.tApp e1 e2) as e_app in H.
+    remember (Val v_app) as r_app in H.
+    inversion H; subst; try discriminate.
+    - injection H4 as <- <-. subst.
+      eexists _, _, _. exact H1.
+    - injection H6 as <- <-. subst.
+      eexists _, _, _. exact H3.
+  Qed.
+
+  (** If both prefixes of an application evaluate successfully, then successful
+      or divergent whole-app behavior projects to the closure body. *)
+  Lemma src_eval_app_val_body rho e1 e2 rho' na body v2 v_app
+        f1 t1 f2 t2 f t :
+    eval_env_fuel rho e1 (Val (Clos_v rho' na body)) f1 t1 ->
+    eval_env_fuel rho e2 (Val v2) f2 t2 ->
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app) f t ->
+    exists f3 t3,
+      eval_env_fuel (v2 :: rho') body (Val v_app) f3 t3.
+  Proof.
+    intros He1 He2 Happ.
+    inversion Happ; subst; try discriminate.
+    remember (EAst.tApp e1 e2) as e_app in H.
+    remember (Val v_app) as r_app in H.
+    inversion H; subst; try discriminate.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He1 H0)
+        as [Heq1 [-> ->]].
+      injection Heq1 as <- <- <-.
+      pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He2 H1)
+        as [-> [-> ->]].
+      match goal with
+      | [ Hbody : eval_env_fuel (_ :: _) _ (Val _) ?fb ?tb |- _ ] =>
+          exists fb, tb; exact Hbody
+      end.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He1 H0)
+        as [Heq1 _].
+      discriminate.
+  Qed.
+
+  Lemma src_eval_app_val_of_body
+        rho e1 e2 rho' na body v2 v_app f1 t1 f2 t2 f3 t3 :
+    eval_env_fuel rho e1 (Val (Clos_v rho' na body)) f1 t1 ->
+    eval_env_fuel rho e2 (Val v2) f2 t2 ->
+    eval_env_fuel (v2 :: rho') body (Val v_app) f3 t3 ->
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app)
+                  (f1 <+> f2 <+> f3 <+> one_i (EAst.tApp e1 e2))
+                  (t1 <+> t2 <+> t3 <+> one_i (EAst.tApp e1 e2)).
+  Proof.
+    intros He1 He2 Hbody.
+    econstructor. econstructor; eauto.
+  Qed.
+
+  Lemma src_eval_fixapp_val_body rho e1 e2 rho' idx na mfix body v2 v_app
+        f1 t1 f2 t2 f t :
+    eval_env_fuel rho e1 (Val (ClosFix_v rho' mfix idx)) f1 t1 ->
+    fix_body mfix idx = Some (EAst.tLambda na body) ->
+    eval_env_fuel rho e2 (Val v2) f2 t2 ->
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app) f t ->
+    exists f3 t3,
+      eval_env_fuel (v2 :: make_rec_env mfix rho') body (Val v_app) f3 t3.
+  Proof.
+    intros He1 Hfix He2 Happ.
+    rename Hfix into Hfix_saved.
+    inversion Happ; subst; try discriminate.
+    remember (EAst.tApp e1 e2) as e_app in H.
+    remember (Val v_app) as r_app in H.
+    inversion H; subst; try discriminate.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He1 H0)
+        as [Heq1 _].
+      discriminate.
+    - match goal with
+      | [ Heq : EAst.tApp _ _ = EAst.tApp _ _ |- _ ] =>
+          injection Heq as <- <-; subst
+      end.
+      lazymatch goal with
+      | [ He1_2 : eval_env_fuel _ _ (Val (ClosFix_v _ _ _)) _ _,
+          Hfix2 : fix_body _ _ = Some (EAst.tLambda _ _),
+          He2_2 : eval_env_fuel _ _ (Val _) _ _,
+          Hbody : eval_env_fuel (_ :: make_rec_env _ _) _ (Val _) ?fb ?tb |- _ ] =>
+          pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He1 He1_2)
+            as [Heq1 [-> ->]];
+          injection Heq1 as Hrho Hmfix Hidx;
+          rewrite <- Hidx in Hfix2;
+          rewrite <- Hmfix in Hfix2;
+          rewrite <- Hrho in Hbody;
+          rewrite <- Hmfix in Hbody;
+          rewrite Hfix_saved in Hfix2; injection Hfix2 as <- <-;
+          pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ He2 He2_2)
+            as [-> [-> ->]];
+          exists fb, tb; exact Hbody
+      end.
+  Qed.
+
+  Lemma src_eval_fixapp_val_of_body
+        rho e1 e2 rho' idx na mfix body v2 v_app f1 t1 f2 t2 f3 t3 :
+    eval_env_fuel rho e1 (Val (ClosFix_v rho' mfix idx)) f1 t1 ->
+    fix_body mfix idx = Some (EAst.tLambda na body) ->
+    eval_env_fuel rho e2 (Val v2) f2 t2 ->
+    eval_env_fuel (v2 :: make_rec_env mfix rho') body (Val v_app) f3 t3 ->
+    eval_env_fuel rho (EAst.tApp e1 e2) (Val v_app)
+                  (f1 <+> f2 <+> f3 <+> one_i (EAst.tApp e1 e2))
+                  (t1 <+> t2 <+> t3 <+> one_i (EAst.tApp e1 e2)).
+  Proof.
+    intros He1 Hfix He2 Hbody.
+    econstructor.
+    eapply eval_FixApp_step with (rho'' := make_rec_env mfix rho'); eauto.
+  Qed.
+
+  (** Successful let-evaluations project to successful evaluations of both the
+      bound term and, after fixing that result, the body. *)
+  Lemma src_eval_let_val_bind rho na b t v_let f tr :
+    eval_env_fuel rho (EAst.tLetIn na b t) (Val v_let) f tr ->
+    exists v1 f1 t1,
+      eval_env_fuel rho b (Val v1) f1 t1.
+  Proof.
+    intros Hlet.
+    inversion Hlet; subst; try discriminate.
+    remember (EAst.tLetIn na b t) as e_let in H.
+    remember (Val v_let) as r_let in H.
+    inversion H; subst; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tLetIn _ _ _ = EAst.tLetIn _ _ _ |- _ ] =>
+        injection Heq as <- <- <-; subst
+    end.
+    lazymatch goal with
+    | [ Hb : eval_env_fuel rho _ (Val ?v1) ?f1 ?t1 |- _ ] =>
+        exists v1, f1, t1; exact Hb
+    end.
+  Qed.
+
+  Lemma src_eval_let_val_body rho na b t v1 v_let f1 t1 f tr :
+    eval_env_fuel rho b (Val v1) f1 t1 ->
+    eval_env_fuel rho (EAst.tLetIn na b t) (Val v_let) f tr ->
+    exists f2 t2,
+      eval_env_fuel (v1 :: rho) t (Val v_let) f2 t2.
+  Proof.
+    intros Hb Hlet.
+    inversion Hlet; subst; try discriminate.
+    remember (EAst.tLetIn na b t) as e_let in H.
+    remember (Val v_let) as r_let in H.
+    inversion H; subst; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tLetIn _ _ _ = EAst.tLetIn _ _ _ |- _ ] =>
+        injection Heq as <- <- <-; subst
+    end.
+    lazymatch goal with
+    | [ Hb2 : eval_env_fuel rho _ (Val _) _ _,
+        Hbody : eval_env_fuel (_ :: rho) _ (Val _) ?f2 ?t2 |- _ ] =>
+        pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ Hb Hb2)
+          as [-> [-> ->]];
+        exists f2, t2; exact Hbody
+    end.
+  Qed.
+
+  Lemma src_eval_let_val_of_body rho na b t v1 v_let f1 t1 f2 t2 :
+    eval_env_fuel rho b (Val v1) f1 t1 ->
+    eval_env_fuel (v1 :: rho) t (Val v_let) f2 t2 ->
+    eval_env_fuel rho (EAst.tLetIn na b t) (Val v_let)
+                  (f1 <+> f2 <+> one_i (EAst.tLetIn na b t))
+                  (t1 <+> t2 <+> one_i (EAst.tLetIn na b t)).
+  Proof.
+    intros Hbind Hbody.
+    econstructor. econstructor; eauto.
+  Qed.
+
+  (** Successful case/projection evaluations project to successful evaluation of
+      their scrutinized subterm, and of the chosen branch body for cases. *)
+  Lemma src_eval_case_val_scrut rho ind npars mch brs v_case f tr :
+    eval_env_fuel rho (EAst.tCase (ind, npars) mch brs) (Val v_case) f tr ->
+    exists dc vs f1 t1,
+      eval_env_fuel rho mch (Val (Con_v dc vs)) f1 t1.
+  Proof.
+    intros Hcase.
+    inversion Hcase; subst; try discriminate.
+    remember (EAst.tCase (ind, npars) mch brs) as e_case in H.
+    remember (Val v_case) as r_case in H.
+    inversion H; subst; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tCase _ _ _ = EAst.tCase _ _ _ |- _ ] =>
+        injection Heq as <- <- <-; subst
+    end.
+    lazymatch goal with
+    | [ Hmch : eval_env_fuel rho _ (Val (Con_v ?dc ?vs)) ?f1 ?t1 |- _ ] =>
+        exists dc, vs, f1, t1; exact Hmch
+    end.
+  Qed.
+
+  Lemma src_eval_case_val_body rho ind npars mch brs dc vs body c v_case
+        f1 t1 f tr :
+    eval_env_fuel rho mch (Val (Con_v dc vs)) f1 t1 ->
+    dc = dcon_of_con ind c ->
+    find_branch ind c (List.length vs) brs = Some body ->
+    eval_env_fuel rho (EAst.tCase (ind, npars) mch brs) (Val v_case) f tr ->
+    exists f2 t2,
+      eval_env_fuel ((List.rev vs) ++ rho) body (Val v_case) f2 t2.
+  Proof.
+    intros Hmch Hdc Hfind Hcase.
+    inversion Hcase; subst; try discriminate.
+    remember (EAst.tCase (ind, npars) mch brs) as e_case in H.
+    remember (Val v_case) as r_case in H.
+    inversion H; subst; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tCase _ _ _ = EAst.tCase _ _ _ |- _ ] =>
+        injection Heq as <- <- <-; subst
+    end.
+    lazymatch goal with
+    | [ Hmch2 : eval_env_fuel rho _ (Val (Con_v _ _)) _ _,
+        Hfind2 : find_branch _ _ _ _ = Some _,
+        Hbody : eval_env_fuel ((List.rev _) ++ rho) _ (Val _) ?f2 ?t2 |- _ ] =>
+        pose proof (eval_val_exact_det _ _ _ _ _ _ _ _ Hmch Hmch2)
+          as [Heq_con [-> ->]];
+        inversion Heq_con; subst;
+        apply Nnat.Nat2N.inj in H4; subst c0;
+        rewrite Hfind in Hfind2; injection Hfind2 as <-;
+        exists f2, t2; exact Hbody
+    end.
+  Qed.
+
+  Lemma src_eval_case_val_of_body
+        rho ind npars mch brs dc vs body c v_case f1 t1 f2 t2 :
+    eval_env_fuel rho mch (Val (Con_v dc vs)) f1 t1 ->
+    dc = dcon_of_con ind c ->
+    find_branch ind c (List.length vs) brs = Some body ->
+    eval_env_fuel ((List.rev vs) ++ rho) body (Val v_case) f2 t2 ->
+    eval_env_fuel rho (EAst.tCase (ind, npars) mch brs) (Val v_case)
+                  (f1 <+> f2 <+> one_i (EAst.tCase (ind, npars) mch brs))
+                  (t1 <+> t2 <+> one_i (EAst.tCase (ind, npars) mch brs)).
+  Proof.
+    intros Hmch Hdc Hfind Hbody.
+    econstructor. econstructor; eauto.
+  Qed.
+
+  Lemma src_eval_proj_val_scrut rho p c v f tr :
+    eval_env_fuel rho (EAst.tProj p c) (Val v) f tr ->
+    exists vs f1 t1,
+      eval_env_fuel rho c
+        (Val (Con_v (dcon_of_con p.(proj_ind) 0) vs)) f1 t1.
+  Proof.
+    intros Hproj.
+    inversion Hproj; subst; try discriminate.
+    remember (EAst.tProj p c) as e_proj in H.
+    remember (Val v) as r_proj in H.
+    inversion H; subst; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tProj _ _ = EAst.tProj _ _ |- _ ] =>
+        injection Heq as <- <-; subst
+    end.
+    lazymatch goal with
+      | [ Hc : eval_env_fuel rho _ (Val (Con_v _ ?vs)) ?f1 ?t1 |- _ ] =>
+          exists vs, f1, t1; exact Hc
+      end.
+  Qed.
+
+  Lemma src_eval_proj_val_of_scrut rho p c vs v f1 t1 :
+    eval_env_fuel rho c (Val (Con_v (dcon_of_con p.(proj_ind) 0) vs)) f1 t1 ->
+    nth_error vs p.(proj_arg) = Some v ->
+    eval_env_fuel rho (EAst.tProj p c) (Val v)
+                  (f1 <+> one_i (EAst.tProj p c))
+                  (t1 <+> one_i (EAst.tProj p c)).
+  Proof.
+    intros Hscrut Hnth.
+    econstructor. econstructor; eauto.
+  Qed.
+
+  Lemma src_eval_construct_val_arg rho ind c args_done e args_rest v_con f t :
+    eval_env_fuel rho (EAst.tConstruct ind c (args_done ++ e :: args_rest))
+                  (Val v_con) f t ->
+    exists v f_e t_e, eval_env_fuel rho e (Val v) f_e t_e.
+  Proof.
+    intros Hcon.
+    remember (EAst.tConstruct ind c (args_done ++ e :: args_rest)) as e_con in Hcon.
+    remember (Val v_con) as r_con in Hcon.
+    destruct Hcon; try discriminate. subst.
+    remember (EAst.tConstruct ind c (args_done ++ e :: args_rest)) as e_con in H.
+    remember (Val v_con) as r_con in H.
+    destruct H; try discriminate.
+    lazymatch goal with
+    | [ Heq : EAst.tConstruct _ _ _ = EAst.tConstruct _ _ _ |- _ ] =>
+        inversion Heq; subst; clear Heq
+    end.
+    edestruct eval_many_app_inv as
+      (vs_done & v & vs_rest & fs1 & f_e & fs2 & ts1 & t_e & ts2 &
+       _ & _ & Heval_e & _ & _ & _).
+    { exact H0. }
+    exists v, f_e, t_e. exact Heval_e.
   Qed.
 
 End FUEL_SEM.
