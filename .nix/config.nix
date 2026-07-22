f253cbc751ab5064b541d87d11c2a3e71f1be3ae
Félix SASSUS BOURDA <felix.sassus-bourda@proton.me>
2026-07-22 14:02:18 +0200

    Changed to the correct metarocq branch

--- .nix/config.nix
+++ .nix/config.nix
@@ -28,7 +28,7 @@
       ExtLib.job = false;
       ExtLib.override.version = "0.13.0";
       metarocq.job = true;
-      metarocq.override.version = "9.1";
+      metarocq.override.version = "9.1-eval-step-index";
     }; rocqPackages = {
       rocq-core.override.version = "9.1";
     };
